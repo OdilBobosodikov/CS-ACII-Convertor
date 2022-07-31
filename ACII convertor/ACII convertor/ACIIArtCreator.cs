23644e4c2afbedd9adb3737d087218b59079aaf6
OdilBobosodikov <110328500+OdilBobosodikov@users.noreply.github.com>
2022-07-31 13:18:28 +0500

    Add files via upload
    ACII convertor. 
    Converts an image to ACII text file

---
+++ ACII convertor/ACII convertor/ACIIArtCreator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace ACII_convertor
+{
+    public class ACIIArtCreator
+    {
+        private string imageLocation;
+        private string _pixels = " .-+*wGHM#&%";
+        public ACIIArtCreator(string imageLoication)
+        {
+                this.imageLocation = imageLoication;
+        }
+
+        public void Go(string saveLocation)
+        {
+            var Image = new Bitmap(imageLocation);
+            
+            using (var wtr = new StreamWriter(saveLocation))
+            {
+                for (int y = 0; y < Image.Height; y++)
+                {
+                    for (int x = 0; x < Image.Width; x++)
+                    {
+                        var color = Image.GetPixel(x, y);
+                        var brightness = Brightness(color);
+                        var idx = brightness / 255 * (_pixels.Length - 1);
+                        var pxl = _pixels[_pixels.Length - (int)Math.Round(idx) - 1];
+                        wtr.Write(pxl);
+                    }
+                    wtr.WriteLine();
+                }
+            }
+        }
+
+        private static double Brightness(Color c)
+        {
+            return Math.Sqrt(
+               c.R * c.R * .241 +
+               c.G * c.G * .691 +
+               c.B * c.B * .068);
+        }
+    }
+}
---
+++ ACII convertor/ACII convertor/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ACII_convertor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new ACIIArtCreator("C:/Users/Shukrullo/Desktop/test3.jpg");
+            test.Go("C:/Users/Shukrullo/Desktop/ACII.txt");
+        }
+    }
+}
